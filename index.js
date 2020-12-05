6c73c99f5930654ba006e0ede9d97fb3fc62f87d
sang29 <sangik.han29@gmail.com>
2020-12-04 21:05:26 -0500

    changed greeting

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express'); //this is like import
 const app = express(); //generating new application called app
 
 app.get('/', (req, res) => {//route handler
-    res.send({ hi: 'there'});
+    res.send({ bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000; //use 5000 if heroku not available
 app.listen(PORT); //dynamic port
-
